522804468cd77a079884ba0f16673efdb87cf051
kkya <kkya2014@163.com>
2016-01-21 10:49:40 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -496,14 +496,14 @@ gulp.task('watch', function(cb) {
 gulp.task('connect', function(cb) {
     connect.server({
         root: [paths.root],
-        port: '3002'
+        port: '3003'
     });
     cb();
 });
 
 gulp.task('open', function(cb) {
     gulp.src(paths.examples.index).pipe(open('', {
-        url: 'http://localhost:3002/' + paths.examples.index
+        url: 'http://localhost:3003/' + paths.examples.index
     }));
     cb();
 });
